519556737082b3663d587993dea9fb22cba3ea17
Ujjwal Arora <ukarora9@gmail.com>
2025-03-10 00:10:12 +0530

    Initial Commit

---
+++ XCodeCLoudWesChua1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XCodeCLoudWesChua1
+//
+//  Created by Ujjwal Arora on 10/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XCodeCLoudWesChua1/XCodeCLoudWesChua1App.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeCLoudWesChua1App.swift
+//  XCodeCLoudWesChua1
+//
+//  Created by Ujjwal Arora on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct XCodeCLoudWesChua1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
